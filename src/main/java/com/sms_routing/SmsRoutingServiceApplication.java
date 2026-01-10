6f64ecaedf3ac939dd085fdd503ed78238ba1f2a
Colton Randall <coltonrandall.nz@gmail.com>
2026-01-11 12:49:01 +1300

    initial java spring boot project setup

---
+++ src/main/java/com/sms_routing/SmsRoutingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sms_routing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsRoutingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmsRoutingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sms_routing/SmsRoutingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sms_routing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsRoutingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
